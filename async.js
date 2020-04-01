1a6766b488ccc870573f27baed544ef1e01e99b8
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-04-01 21:43:31 +0200

    Initial

---
+++ async.js
@@ -0,0 +1,2 @@
+import {UI_EVENT_TYPE} from "./switcher";
+console.log(UI_EVENT_TYPE)
---
+++ index.js
@@ -0,0 +1,4 @@
+import { OPERATIONAL_EVENT_TYPE } from "./types";
+console.log(OPERATIONAL_EVENT_TYPE);
+
+import("./async");
